fbdccf72ac12942623b9b8d76e8f9241dfb9b334
Sk Rohul Amin <skrohulamin@0109@gmail.com>
2026-07-13 12:15:20 +0530

    fix a route in routes/userRouter.js

--- Notes App/backend/routes/userRoutes.js
+++ Notes App/backend/routes/userRoutes.js
@@ -12,7 +12,7 @@ const verifyToken = require("../middlewares/authMiddleware");
 userRouter.route("/:userId").get(verifyToken, getAllNotes);
 userRouter.route("/:userId/create-note").post(verifyToken, createNote);
 userRouter
-    .route("/userId/:noteId")
+    .route("/:userId/:noteId")
     .get(verifyToken, getNoteById)
     .put(verifyToken, updateNote)
     .delete(verifyToken, deleteNote);
